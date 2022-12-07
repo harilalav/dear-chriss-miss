356551fcf548deac1ff3cec8bea8387f9087e6d1
Harilal AV <harilalav@Harilals-MacBook-Air.local>
2022-12-07 21:33:26 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var captionLength = 0;
+var caption = '';
+
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+    captionEl = $('#content');
+    testTypingEffect();
+});
+
+function testTypingEffect() {
+    caption = "Dear Miss Chriss,<br /> Thank you for assisting us in gaining self-assurance.<br /> We had your full support and flexibility to help us become capable. <br /> It's impossible to express the confidence boost you gave us by constantly taking us into account throughout events. <br /> The encouraging remarks provided us the courage and strength to pursue the life we had always wanted..<br /> We will never forget our time in your class. you helped us to think bigger, understand the basics, and you are one of the people who has led us to where we are today. <br /> We are so thankful for having a teacher like you, we wish you the best of luck on your next endeavors.<br /> Hope to see you at some other point in life.<br /> Travel Safe :)";
+    type();
+}
+
+function type() {
+    captionEl.html(caption.substr(0, captionLength++));
+    if(captionLength < caption.length+1) {
+        setTimeout('type()', 80);
+    } else {
+        captionLength = 0;
+        caption = '';
+    }
+}
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
